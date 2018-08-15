5579e10cc23b8cb12c4378fb41e986fa3212ecfc
Ignacio Rivas <rivasign@gmail.com>
2018-08-15 20:04:11 +0200

    remove link

--- src/components/image/index.js
+++ src/components/image/index.js
@@ -6,9 +6,7 @@ import styles from './styles.module.scss';
 
 export default ({ img, alt, small }) => (
   <div className={styles['new-image-wrapper']}>
-    <a href={img}>
-      <img src={img} alt={alt} />
-    </a>
+    <img src={img} alt={alt} />
     <small>{small}</small>
   </div>
 );
