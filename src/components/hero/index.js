7fd31b608ecb763950cc2cf5a27b6b1b9262d3d2
Ignacio Rivas <rivasign@gmail.com>
2018-08-20 18:24:26 +0200

    Small copy fix

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -7,7 +7,7 @@ import styles from './styles.module.scss';
 export default () => (
   <div className={styles.root}>
     <h1>
-      <div style={{ marginBottom: 25 }}>Hi, it's Vaiva.</div>
+      <div style={{ marginBottom: 25 }}>Hi, I'm Vaiva.</div>
       I'm a UX designer dedicated to create positive experiences for people.
     </h1>
   </div>
